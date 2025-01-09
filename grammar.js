c92b4d82dae1d3a269ce843e1ae3f3cb9e34993a
Matt LeMay <mplemay@users.noreply.github.com>
2025-01-08 20:40:13 -0600

    feat: added python to the grammar

--- grammar.js
+++ grammar.js
@@ -7,11 +7,32 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
-module.exports = grammar({
+const python = require('tree-sitter-python/grammar');
+
+module.exports = grammar(python, {
   name: "pyjsx",
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => seq(
+      repeat(choice($.jsx_element, $.jsx_fragment, $._statement))
+    ),
+
+    jsx_element: $ => seq(
+      "<",
+      $.element_name,
+      ">",
+      repeat(choice($.jsx_element, $.jsx_fragment, $._statement)),
+      "</",
+      $.element_name,
+      ">"
+    ),
+
+    jsx_fragment: $ => seq(
+      "<>",
+      repeat(choice($.jsx_element, $.jsx_fragment, $._statement)),
+      "</>"
+    ),
+
+    element_name: $ => /[A-Za-z_]\w*(?:\.[A-Za-z_]\w*)*/
   }
 });
