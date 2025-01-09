e05269f4f51510ae348cc1cb8e6d9e5fc7a5fcb5
Matt LeMay <mplemay@users.noreply.github.com>
2025-01-09 06:01:24 -0600

    feat: reset grammar to python specification

--- src/parser.c
+++ src/parser.c
@@ -130138,11 +130138,11 @@ static const bool ts_external_scanner_states[20][EXTERNAL_TOKEN_COUNT] = {
 #ifdef __cplusplus
 extern "C" {
 #endif
-void *tree_sitter_pyjsx_external_scanner_create(void);
-void tree_sitter_pyjsx_external_scanner_destroy(void *);
-bool tree_sitter_pyjsx_external_scanner_scan(void *, TSLexer *, const bool *);
-unsigned tree_sitter_pyjsx_external_scanner_serialize(void *, char *);
-void tree_sitter_pyjsx_external_scanner_deserialize(void *, const char *, unsigned);
+void *tree_sitter_python_external_scanner_create(void);
+void tree_sitter_python_external_scanner_destroy(void *);
+bool tree_sitter_python_external_scanner_scan(void *, TSLexer *, const bool *);
+unsigned tree_sitter_python_external_scanner_serialize(void *, char *);
+void tree_sitter_python_external_scanner_deserialize(void *, const char *, unsigned);
 
 #ifdef TREE_SITTER_HIDE_SYMBOLS
 #define TS_PUBLIC
@@ -130152,7 +130152,7 @@ void tree_sitter_pyjsx_external_scanner_deserialize(void *, const char *, unsign
 #define TS_PUBLIC __attribute__((visibility("default")))
 #endif
 
-TS_PUBLIC const TSLanguage *tree_sitter_pyjsx(void) {
+TS_PUBLIC const TSLanguage *tree_sitter_python(void) {
   static const TSLanguage language = {
     .version = LANGUAGE_VERSION,
     .symbol_count = SYMBOL_COUNT,
@@ -130183,11 +130183,11 @@ TS_PUBLIC const TSLanguage *tree_sitter_pyjsx(void) {
     .external_scanner = {
       &ts_external_scanner_states[0][0],
       ts_external_scanner_symbol_map,
-      tree_sitter_pyjsx_external_scanner_create,
-      tree_sitter_pyjsx_external_scanner_destroy,
-      tree_sitter_pyjsx_external_scanner_scan,
-      tree_sitter_pyjsx_external_scanner_serialize,
-      tree_sitter_pyjsx_external_scanner_deserialize,
+      tree_sitter_python_external_scanner_create,
+      tree_sitter_python_external_scanner_destroy,
+      tree_sitter_python_external_scanner_scan,
+      tree_sitter_python_external_scanner_serialize,
+      tree_sitter_python_external_scanner_deserialize,
     },
     .primary_state_ids = ts_primary_state_ids,
   };
